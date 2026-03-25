c3bc1e5b93792379a622bc8c62d498e087d3f506
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-03-25 20:44:42 +0600

    Remove unused LoginDto import from AuthService

--- auth.service.ts
+++ auth.service.ts
@@ -3,7 +3,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../users/entities/user.entity';
 import { EmailService } from './email.service';
-import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
