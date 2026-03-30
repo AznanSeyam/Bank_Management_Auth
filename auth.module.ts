bc7eec42623c1413eebcfb3f8b6439d4bd4880cd
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-03-30 20:36:54 +0600

    Remove User import from auth.module.ts

--- auth.module.ts
+++ auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { User } from '../users/entities/user.entity';
 
 @Module({
   imports: [UsersModule, TypeOrmModule.forFeature([User])],
