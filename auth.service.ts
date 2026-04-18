a2578f80e7cc205bc5c1ff3bc141cd48ae950e34
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-04-18 16:05:23 +0600

    Remove unused EmailService import from AuthService

--- auth.service.ts
+++ auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
-import { EmailService } from './email.service';
 
 @Injectable()
 export class AuthService {
