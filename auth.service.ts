97e23f1d0e3f3510abbfd3ca16b55f205b2218a3
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-04-24 10:08:42 +0600

    Remove unused InjectRepository import

--- auth.service.ts
+++ auth.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common'
-import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
 
 @Injectable()
