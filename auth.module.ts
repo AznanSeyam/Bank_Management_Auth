836a3d88a597d526bad2dd4ed591262334bf1e94
Aznan Seyam <106177845+AznanSeyam@users.noreply.github.com>
2026-07-10 00:34:01 +0600

    Update auth.module.ts

--- auth.module.ts
+++ auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller'
-
 @Module({
   imports: [UsersModule, TypeOrmModule.forFeature([User])],
   providers: [AuthService, EmailService],
